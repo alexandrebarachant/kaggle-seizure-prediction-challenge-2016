00f937cc7710977dc812d9fc675864e2b8288658
bee <lixx4699@umn.edu>
2016-12-09 14:54:15 -0600

    Update config_name_creator.py

--- Feng/utils/config_name_creator.py
+++ Feng/utils/config_name_creator.py
@@ -11,14 +11,3 @@ def create_fft_data_name(settings_dict):
            + 'win_length_sec' + str(settings_dict['preprocessor']['win_length_sec']) \
            + 'stride_sec' + str(settings_dict['preprocessor']['stride_sec'])
 
-
-def create_cnn_model_name(settings_dict):
-    config_name = ''
-    for key, value in settings_dict['preprocessor'].items():
-        config_name += key[:5] + str(value)
-    for key, value in settings_dict['model'].items():
-        config_name += key[:5] + str(value)
-    for key, value in settings_dict['validation'].items():
-        config_name += key[:5] + str(value)
-    config_name = config_name.replace('\'','')
-    return config_name
